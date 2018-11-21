c15a51f2bae915d7ed8c2af93718b2960a9b1e4e
ismata <chufadar@it-partner.bg>
2018-11-21 15:57:22 +0200

    asdasdasd

--- catalog/model/extension/shipping/econt_delivery.php
+++ catalog/model/extension/shipping/econt_delivery.php
@@ -3,6 +3,7 @@
 class ModelExtensionShippingEcontDelivery extends Model {
 
     function getQuote($address) {
+        // TODO: kopie na flat.php kolkoto da ne se chupi saita
         $this->load->language('extension/shipping/flat');
 
         $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('shipping_flat_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
